5eaf87d3a34c05a572b29d2ca83a7f11ce74b18c
Leo Souza <leandrosilvadesouza6397@gmail.com>
2026-04-18 16:55:42 -0300

    Add files via upload
    Na última atualização, o método de animação de cartas indisponíveis na classe AnimaçãoTabuleiro deixou de funcionar por entrar em conflito com o hover da classe Cartas, nessa versão isso foi atualizado

---
+++ Alfa_01/Versions/game 2/ClassesBase/Inimigo.java
@@ -0,0 +1,10 @@
+package com.root.game.ClassesBase;
+
+public abstract class Inimigo extends Entidade {
+
+    protected String nome;
+
+    public String getNome() {
+        return nome;
+    }
+}
---
+++ Alfa_01/Versions/game 2/Inimigos/Esqueleto.java
@@ -0,0 +1,12 @@
+package com.root.game.Inimigos;
+
+import com.root.game.ClassesBase.Inimigo;
+
+public class Esqueleto extends Inimigo {
+    public Esqueleto() {
+        nome = "Esqueleto";
+        vida = 20;
+        ataque = 5;
+        defesa = 2;
+    }
+}
